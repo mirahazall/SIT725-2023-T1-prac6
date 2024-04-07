e9fce89623b94628abf86de7ae8e774d096e45f9
mirahazall <s223530601@deakin.edu.au>
2024-04-08 07:50:55 +1000

    eslint

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+// Your JavaScript code here
+
+module.exports = {
+    // Other ESLint configurations
+    rules: {
+      // Other rules...
+      // Ignore specific rules by setting them to "off" or 0
+      'no-undef': 'off', // Ignore "no-undef" rule
+      '@typescript-eslint/no-var-requires': 'off', // Ignore "@typescript-eslint/no-var-requires" rule
+      // Other rules...
+    }
+  };
