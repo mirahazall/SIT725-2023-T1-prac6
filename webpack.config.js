cbc3645f5cb84eacbc6d6b0a055e6a05165bc1e4
mirahazall <s223530601@deakin.edu.au>
2024-04-07 12:56:34 +1000

    commit

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
  mode: 'development', // Set the mode option
-  entry: '../../public/js/scripts.js',
+  entry: './public/js/scripts.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
