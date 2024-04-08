c06cb3fb270665f0b1b389d4193439c085d2a7e2
mirahazall <s223530601@deakin.edu.au>
2024-04-08 12:14:01 +1000

    commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,38 +1,17 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-
-export default [
-  // Configuration for JavaScript files
-  {
-    files: ["**/*.js"],
-    languageOptions: {
-      sourceType: "commonjs",
-      globals: globals.browser // Define global variables
-    },
-    plugins: {
-      "@eslint/js": pluginJs
+module.exports = {
+    sourceType: "module", // Specify source type as module
+    parserOptions: {
+      ecmaVersion: 2022, // Specify ECMAScript version
     },
     rules: {
       'no-unused-vars': 'off', // Ignore unused variables
       'no-undef': 'off', // Ignore undefined variables
       '@typescript-eslint/no-var-requires': 'off', // Ignore require statements
       '@typescript-eslint/no-unused-vars': 'off' // Ignore unused variables in TypeScript code
-      // Add other JavaScript-specific rules here if needed
+      // Add other rules here if needed
     }
-  },
-  // Configuration for TypeScript files
-  {
-    files: ["**/*.ts", "**/*.tsx"],
-    plugins: {
-      "typescript-eslint": tseslint
-    },
-    rules: {
-      // TypeScript-specific rules
-      // Add other TypeScript-specific rules here if needed
-    }
-  }
-];
+  };
+  
 
 
 
