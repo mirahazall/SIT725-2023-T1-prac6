f6c7678e9d58d3142d96a6fc25bd743de2f64208
mirahazall <s223530601@deakin.edu.au>
2024-04-08 12:00:19 +1000

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,26 @@
-import globals from "globals"; 
-import pluginJs from "@eslint/js"; 
-import tseslint from "typescript-eslint"; 
-export default [ {files: ["**/*.js"],
- languageOptions: {sourceType: "commonjs"}}, 
- {languageOptions: { globals: globals.browser }}, 
- pluginJs.configs.recommended, ...tseslint.configs.recommended, ];
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default [
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "commonjs",
+      globals: {
+        browser: true // Define your global variables here
+      }
+    },
+    plugins: ["@eslint/js", "typescript-eslint"],
+    extends: ["plugin:@eslint/js/recommended", "plugin:typescript-eslint/recommended"],
+    rules: {
+      'no-unused-vars': 'off', // Ignore unused variables
+      'no-undef': 'off', // Ignore undefined variables
+      '@typescript-eslint/no-var-requires': 'off', // Ignore require statements
+      '@typescript-eslint/no-unused-vars': 'off' // Ignore unused variables in TypeScript code
+      // Add other rules here if needed
+    }
+  }
+];
+
 
   
